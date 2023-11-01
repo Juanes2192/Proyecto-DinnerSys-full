55e27ff2d9219bc0355df9660d48178e2255a144
Juanes2192 <juan.perdomo01@unicatolica.edu.co>
2023-11-01 01:21:01 -0500

    Se agrega parte visual al listado de usuarios

--- FrontEnd/src/pages/Admin/MenusAdmin/MenuUsuariosAdmin/OpcionesUsuariosAdmin/ListadoUsuariosAdmin/ListadoUsuariosAdmin.js
+++ FrontEnd/src/pages/Admin/MenusAdmin/MenuUsuariosAdmin/OpcionesUsuariosAdmin/ListadoUsuariosAdmin/ListadoUsuariosAdmin.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import "./ListadoUsuariosAdmin.css"
 import axios from 'axios';
 import { BASE_API } from '../../../../../../utils/constants';
@@ -7,32 +7,35 @@ export function ListadoUsuariosAdmin() {
 
   const [data, setData] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     axios.get(`${BASE_API}/Usuarios/MostrarUsuarios`)
       .then((response) => {
         console.log(response.data);
         setData(response.data);
       })
-      .catch((error)=>{
+      .catch((error) => {
         console.error("Error al traer datos: ", error);
       })
-  },[])
+  }, [])
 
   return (
-    <div>
-        <h1>ListadoUsuariosAdmin</h1>
-        <ul>
-          {data.map((user)=>
-            <div key={user.id}> 
-              <ul> 
-                <li> {user.Nombre} <br/> {user.Rol} </li>
-                <br/>
-              </ul>
-            </div>
-          )}
-        </ul>
-
+    <div className="container">
+    <h1 className='Titulo'>Listado de Usuarios</h1>
+    <div className="user-list-container">
+      <ul className="user-list">
+        <li className="user-item user-header">
+          <span className="user-label">Nombre</span>
+          <span className="user-label">Rol</span>
+        </li>
+        {data.map((user) => (
+          <li key={user.id} className="user-item">
+            <div className="user-name">{user.Nombre}</div>
+            <div className="user-role">{user.Rol}</div>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  </div>
+  );
 }
 
