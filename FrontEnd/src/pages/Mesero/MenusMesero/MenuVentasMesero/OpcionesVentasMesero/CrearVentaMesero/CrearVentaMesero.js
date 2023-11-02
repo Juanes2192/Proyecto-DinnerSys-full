97ccf7d46b39b0232d2a49d22041c2286318af1b
Juanes2192 <juan.perdomo01@unicatolica.edu.co>
2023-11-02 02:43:48 -0500

    Se agrega seleccion de pedidos al formulario crear venta

--- FrontEnd/src/pages/Mesero/MenusMesero/MenuVentasMesero/OpcionesVentasMesero/CrearVentaMesero/CrearVentaMesero.js
+++ FrontEnd/src/pages/Mesero/MenusMesero/MenuVentasMesero/OpcionesVentasMesero/CrearVentaMesero/CrearVentaMesero.js
@@ -13,50 +13,43 @@ export function CrearVentaMesero() {
             console.log(formValue);
         }
     });
-  return (
-    <div className='form-ventas'>
+
+    const lstProductos = [ // Define tu lista de productos aquí o cárgala desde algún lugar.
+        { id: 1, Producto: "Producto 1", Precio: 10 },
+        { id: 2, Producto: "Producto 2", Precio: 15 },
+        // ... Añade más productos aquí
+    ];
+
+    function getPrecioTotal(producto, isChecked) {
+        // Realiza la lógica para calcular el precio total o realizar acciones relacionadas con los productos seleccionados.
+    }
+
+    return (
+        <div className="container">
+        <div className='form-ventas'>
+            
             <Form className='create-ventas-form' onSubmit={formik.handleSubmit}>
                 <h1>Crear Venta</h1>
                 <Form.Field>
-                    <label>Código Venta</label>
+                    <label>Nombre del Cliente</label>
                     <Input
-                        name="codigoProducto"
-                        placeholder="Código del Producto"
-                        value={formik.values.codigoProducto}
+                        name="nombreCliente"
+                        placeholder="Nombre del cliente"
+                        value={formik.values.nombreCliente}
                         onChange={formik.handleChange}
                     />
-                    {formik.errors.codigoProducto && <div className="error">{formik.errors.codigoProducto}</div>}
+                    {formik.errors.nombreCliente && <div className="error">{formik.errors.nombreCliente}</div>}
                 </Form.Field>
                 <Form.Field>
-                    <label>Nombre del Producto</label>
+                    <label>Cantidad de Clientes</label>
                     <Input
-                        name="nombreProducto"
-                        placeholder="Nombre del Producto"
-                        value={formik.values.nombreProducto}
-                        onChange={formik.handleChange}
-                    />
-                    {formik.errors.nombreProducto && <div className="error">{formik.errors.nombreProducto}</div>}
-                </Form.Field>
-                <Form.Field>
-                    <label>Cantidad</label>
-                    <Input
-                        name="cantidad"
+                        name="cantidadCliente"
                         type="number"
-                        placeholder="Cantidad"
-                        value={formik.values.cantidad}
+                        placeholder="Cantidad de Clientes"
+                        value={formik.values.cantidadCliente}
                         onChange={formik.handleChange}
                     />
-                    {formik.errors.cantidad && <div className="error">{formik.errors.cantidad}</div>}
-                </Form.Field>
-                <Form.Field>
-                    <label>Categoría</label>
-                    <Input
-                        name="categoria"
-                        placeholder="Categoría"
-                        value={formik.values.categoria}
-                        onChange={formik.handleChange}
-                    />
-                    {formik.errors.categoria && <div className="error">{formik.errors.categoria}</div>}
+                    {formik.errors.cantidadCliente && <div className="error">{formik.errors.cantidadCliente}</div>}
                 </Form.Field>
                 <Form.Field>
                     <label>Precio</label>
@@ -71,33 +64,36 @@ export function CrearVentaMesero() {
                 </Form.Field>
                 <Button type="submit">Crear Venta</Button>
             </Form>
+
+            <Form className="formlstProductos">
+                <h2 className="Linealh2">Listado Productos</h2>
+                <div className="carrusel-pedidos">
+                    {lstProductos && lstProductos.map(producto => (
+                        <div key={producto.id}>
+                            <label htmlFor={`producto${producto.id}`}>{producto.Producto + ': $'}<b> {producto.Precio} </b></label>
+                            <input id={`producto${producto.id}`} type='checkbox' onClick={(ev) => getPrecioTotal(producto, ev.target.checked)} />
+                            <br />
+                        </div>
+                    ))}
+                </div>
+            </Form>
         </div>
-  );
-  function initialValues() {
-    return {
-        codigoProducto: "",
-        nombreProducto: "",
-        cantidad: 0,
-        categoria: "",
-        precio: 0,
-    };
-  }
+        </div>
+    );
 
-  function validationSchema() {
-    return Yup.object({
-        codigoProducto: Yup.string().required("El código del producto es obligatorio"),
-        nombreProducto: Yup.string().required("El nombre del producto es obligatorio"),
-        cantidad: Yup.number().required("La cantidad es obligatoria").min(1, "La cantidad debe ser mayor que cero"),
-        categoria: Yup.string().required("La categoría es obligatoria"),
-        precio: Yup.number().required("El precio es obligatorio").min(1, "El precio debe ser mayor que cero"),
-    });
+    function initialValues() {
+        return {
+            nombreCliente: "",
+            cantidadCliente: 0,
+            precio: 0,
+        };
+    }
+
+    function validationSchema() {
+        return Yup.object({
+            nombreCliente: Yup.string().required("El nombre del cliente es obligatorio"),
+            cantidadCliente: Yup.number().required("La cantidad es obligatoria").min(1, "La cantidad debe ser mayor que cero"),
+            precio: Yup.number().required("El precio es obligatorio").min(1, "El precio debe ser mayor que cero"),
+        });
+    }
 }
-
-
-}
-
-
-///////
-
-    
-    
